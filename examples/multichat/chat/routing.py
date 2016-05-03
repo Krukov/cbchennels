9ceaccf715be2405ebf2b429764c3f730f9ddc39
Krukov Dima <glebov.ru@gmail.com>
2016-05-03 23:50:51 +0000

    Cleanup

--- examples/multichat/chat/routing.py
+++ examples/multichat/chat/routing.py
@@ -1,8 +1,5 @@
-from channels import route
 from .consumers import ChatConsumers
 
-# There's no path matching on these routes; we just rely on the matching
-# from the top-level routing. We _could_ path match here if we wanted.
 routing = [
     ChatConsumers.as_routes()
 ]
