94db3843c2575d8ca037e40e0e67c72795a50e60
Krukov Dima <glebov.ru@gmail.com>
2016-07-08 21:39:13 +0000

    Added to serializers multiply serializing (many option)

--- cbchannels/generic/serializers.py
+++ cbchannels/generic/serializers.py
@@ -7,16 +7,20 @@ from django.forms.models import model_to_dict
 
 class SimpleSerializer(object):
 
-    def __init__(self, instance=None, data=None, **kwargs):
+    def __init__(self, instance=None, data=None, many=False, **kwargs):
         self.instance = instance
         self._data = data
         self._validated_data = None
+        self.many = many
         self.kwargs = kwargs
 
     @property
     def data(self):
         if not self._data:
-            data = model_to_dict(self.instance, **self.kwargs)
+            if self.many:
+                data = [model_to_dict(instance, **self.kwargs) for instance in self.instance]
+            else:
+                data = model_to_dict(self.instance, **self.kwargs)
             if data:
                 self._data = json.dumps(data, cls=DjangoJSONEncoder)
         return self._data
@@ -35,7 +39,9 @@ class DjangoSerializer(SimpleSerializer):
     @property
     def data(self):
         if not self._data:
-            self._data = serializers.serialize(self.kwargs.get('format', 'json'), [self.instance], **self.kwargs)
+            self._data = serializers.serialize(self.kwargs.get('format', 'json'),
+                                               self.instance if self.many else [self.instance],
+                                               **self.kwargs)
         return self._data
 
     def is_valid(self):
